9767cdf9770715e7cf7a71db8406a497d74b0758
CI Zimbra <jenkins@zimbra-macmini.synerzipune.local>
2014-11-18 11:48:35 +0530

    test1234

--- TestProject/ViewController.m
+++ TestProject/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"test 1234");
+    NSLog(@"test 1");
 }
 
 - (void)didReceiveMemoryWarning
